5bb668438c3b5eb1a76afb846a2e0db77135f452
Nicollet Njora <nicollet.njora05@gmail.com>
2019-07-21 12:47:05 +0300

    Add a default user avatar

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('profile_img')->nullable();
+            $table->string('profile_img')->default('default-avatar.png');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
